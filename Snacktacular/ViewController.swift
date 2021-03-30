02ee583165d970de57f399e5f2c2928c3df10419
Jay Jung <313jj2@gmail.com>
2021-03-29 22:49:15 -0400

    Initial Commit

---
+++ Snacktacular/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snacktacular
+//
+//  Created by Jay Jung on 3/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
