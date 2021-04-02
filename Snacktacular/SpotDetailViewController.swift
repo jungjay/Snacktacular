7c5ba73d2c3a0904b5b22987a409dc0ee5e0477b
Jay Jung <313jj2@gmail.com>
2021-04-02 15:52:12 -0400

    Section 8.5 – Adding the Spot Class & Saving Data to Cloud Firestore

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -8,24 +8,55 @@
 import UIKit
 
 class SpotDetailViewController: UIViewController {
-
+    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var addressTextField: UITextField!
     @IBOutlet weak var ratingLabel: UILabel!
     
+    var spot: Spot!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        if spot == nil {
+            spot = Spot()
+        }
+        updateUserInterface()
     }
     
-
-    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+    func updateUserInterface() {
+        nameTextField.text = spot.name
+        addressTextField.text = spot.address
+    }
+    
+    func updateFromInterface() {
+        spot.name = nameTextField.text!
+        spot.address = addressTextField.text!
+    }
+    
+    func leaveViewController() {
         let isPresentingInAddMode = presentingViewController is UINavigationController
         if isPresentingInAddMode {
             dismiss(animated: true, completion: nil)
         } else {
             navigationController?.popViewController(animated: true)
         }
+        
     }
     
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        updateFromInterface()
+        spot.saveData { (success) in
+            if success {
+                self.leaveViewController()
+            } else {
+                self.oneButtonAlert(title: "Save Failed", message: "For some reason, the data would not save to the cloud.")
+            }
+        }
+    }
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        leaveViewController()
+    }
 }
+
+
